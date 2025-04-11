fd6ba4fb436fcd9ec1a056116141b57067bd2d30
alcatrazxlr8 <saransh1000@gmail.com>
2025-04-11 15:51:17 -0500

    add comments for better understanding of code

--- points.py
+++ points.py
@@ -1,55 +1,49 @@
 from models import Receipt
 import math
 from datetime import datetime
-import json
 
 def calculate_points(receipt: Receipt) -> int: 
 	points = 0
 
+	# Rule 1: Alphanum chars in retailer name
 	for char in receipt.retailer:
 		if char.isalnum():
 			points += 1
 
-	# if str(receipt.total[-2:]) == "00":
+	# Rule 2: Total amt is a round figure ("35", "35.00")
 	if float(receipt.total) % 1 == 0:
 		points += 50
-		print(points)
-		
+
+	# Rule 3: Total amt is multiple of 0.25
 	if float(receipt.total) % 0.25 == 0:
 		points += 25
-		print(points)
 
+	# Rule 4: 5 points for every 2 items
 	points += 5 * (len(receipt.items) // 2)
-	print(points)
 
+	# Rule 5: Item description is multiple of 3
 	for item in receipt.items:
 		price = float(item.price)
 		if len(item.shortDescription.strip()) % 3 == 0:
 			points += math.ceil(price * 0.2)
-			print(points)
 
-	# date = receipt.purchaseDate.split("-")
-	# if int(date[2]) % 2 == 1:
-	# 	points += 6
-	# 	print(points)
-
+	# Rule 6: Purchased on odd day
 	date = receipt.purchaseDate
 	date = datetime.strptime(date, "%Y-%m-%d").date()
 	receiptDay = date.day
 	if receiptDay % 2 == 1:
 		points += 6
-		print(points)
 
+	# Rule 7: Purchased during a certain time window
 	time = receipt.purchaseTime # time on receipt
-	timeLowerBound = "14:00" # our lower boundary str
-	timeUpperBound = "16:00" # our upper boundary str
+	timeLowerBound = "14:00" # our lower boundary (str)
+	timeUpperBound = "16:00" # our upper boundary (str)
 
-	time = datetime.strptime(time, "%H:%M").time()
 	timeLowerBound = datetime.strptime(timeLowerBound, "%H:%M").time()
 	timeUpperBound = datetime.strptime(timeUpperBound, "%H:%M").time()
+	time = datetime.strptime(time, "%H:%M").time()
 
 	if (timeLowerBound < time < timeUpperBound):
 		points += 10
-		print(points)
 
 	return points
