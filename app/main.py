263c6294e73dcd132652f4726877cd67ebbef754
alcatrazxlr8 <saransh1000@gmail.com>
2025-04-22 19:49:14 -0500

    changed 404 so it is documented in openapi.json

--- app/main.py
+++ app/main.py
@@ -1,32 +1,56 @@
 from fastapi import FastAPI, HTTPException, Request, status
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, RedirectResponse
 from fastapi.exceptions import RequestValidationError
 
 from app.models import Receipt
 from app.storage import receipts_db, points_db
 from app.services import handle_receipt_submission
 
-app = FastAPI()
+app = FastAPI(
+	title="Receipt Processor",
+	description="A simple receipt processor")
+
 
 # Landing page
 @app.get("/")
 def root():
-	return {"message": "Fetch Backend Apprenticeship Assessment"}
+	print ({"message": "Fetch Backend Apprenticeship Assessment"})
+	return RedirectResponse(url="/docs", status_code=status.HTTP_302_FOUND)
+
 
 # Receipt Processing Endpoint
-@app.post("/receipts/process")
+@app.post("/receipts/process", summary="Submits a receipt for processing.")
 def process_receipt(receipt: Receipt):
+	"""
+	Submits a receipt for processing.
+	"""
 	try:
 		receipt_id = handle_receipt_submission(receipt)
 		return {"id": receipt_id}
 	except HTTPException as e:
 		raise e
 
-@app.get("/receipts/{id}/points")
+
+@app.get("/receipts/{id}/points",
+		 summary="Returns the points awarded for the receipt.",
+		 responses={
+		404: {
+			"description": "Receipt not found",
+			"content": {
+				"application/json": {
+					"example": {"detail": "No receipt found"}
+				}
+			}
+		}
+	})
 def get_points(id: str):
+	"""
+	Returns the points awarded for the receipt.
+	"""
 	if id not in receipts_db:
 		raise HTTPException(status_code=404, detail="No receipt found for that ID.")
 	return {"points": points_db[id]}
+
 
 # Catching and overriding the pydantic validation error status
 @app.exception_handler(RequestValidationError)
@@ -36,8 +60,11 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 	for error in errors:
 		field = ".".join(str(loc) for loc in error["loc"] if isinstance(loc, str) and loc != "body")
 		msg = error["msg"]
-		custom_errors.append(f"{field}: {msg}")
+		custom_errors.append(f"The receipt is Invalid => {field}: {msg}")
 	return JSONResponse(
 		status_code=status.HTTP_400_BAD_REQUEST,
-		content={"detail": custom_errors}
+		content={
+			"code": "BAD_REQUEST",
+			"detail": custom_errors
+			}
 	)
